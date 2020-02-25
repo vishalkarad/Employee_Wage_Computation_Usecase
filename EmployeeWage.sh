faf25de0e770fc585ce82068c4e6a5ae4f92a48a
Your Name <you@example.com>
2020-02-25 16:40:06 +0530

    Added : Part time employee wage

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -x
 
+employee_Full_Time_Hours=8
+employee_Part_Time_Hours=4
+emp_Rate_per_Hours=20
+num_Working_Days=20
+mx_Hrs_In_month=100
+employee_hours=0
+
 echo "Welcome to Employee Wage Computation program "
  
 # create random function and check employee is present or not using if else
@@ -9,4 +16,10 @@ then
 	echo "Employee is present "
 else
 	echo "Employee is not present "
+fi
+wage=$(( 20*$employee_hours ))
+
+if [ $employee_Part_Time_Hours -eq 4 ]
+then
+	partTimeEmployeeOneDayWage=$(( $employee_Part_Time_Hours*$emp_Rate_per_Hours ))
 fi
