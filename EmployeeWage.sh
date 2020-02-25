b0e7e64d6585199f8de4fb51ac6353ddab6e551f
Your Name <you@example.com>
2020-02-25 15:46:35 +0530

    Added : Check employee present or absent

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,4 +1,12 @@
-#!/bin/bash
+#!/bin/bash -x
 
-echo "Welcome to Employee Wage Computation program : "
+echo "Welcome to Employee Wage Computation program "
  
+# create random function and check employee is present or not using if else
+random=$(( RANDOM%2 ))
+if [ $random -eq 1 ]
+then
+	echo "Employee is present "
+else
+	echo "Employee is not present "
+fi
