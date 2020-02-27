94fb25436dd63a0c37e2ff88aad118db73b17652
Your Name <you@example.com>
2020-02-27 11:09:54 +0530

    Modified : Changed constant names to capital letters and did indentation properly

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,20 +1,21 @@
 #!/bin/bash -x
 
-#Constant variables
-employee_Full_Time_Hours=8
-employee_Part_Time_Hours=4
-emp_Rate_per_Hours=20
-num_Working_Days=20
-mx_Hrs_In_month=100
-employee_hours=0
-employee_Full_Time_Work=1
-Employee_Part_Time_Work=0
+# Constant variables
+EMP_FULL_TIME_HOURS=8
+EMP_PART_TIME_HOURS=4
+EMP_RATE_PER_HOURS=20
+NUM_WORKING_DAYS=20
+MX_HRS_IN_MONTH=100
+IS_FULL_TIME=1
+IS_PART_TIME=0
 
-#variables
+# variables
+employee_hours=0
 totalWorkHours=0
 totalWorkingDays=0
+
 echo "Welcome to Employee Wage Computation program "
- 
+
 # create random function and check employee is present or not using if else
 random=$(( RANDOM%2 ))
 if [ $random -eq 1 ]
@@ -22,44 +23,50 @@ then
 	echo "Employee is present "
 else
 	echo "Employee is not present "
-fi
-wage=$(( 20*$employee_hours ))
+fi 
 
-if [ $employee_Part_Time_Hours -eq 4 ]
+# Add part time employee and wage
+random1=$(( RANDOM%3 ))
+
+if [ $IS_FULL_TIME -eq $random1 ]
 then
-	partTimeEmployeeOneDayWage=$(( $employee_Part_Time_Hours*$emp_Rate_per_Hours ))
+	empHrs=8
+elif [ $IS_PART_TIME -eq $random1 ]
+then
+	empHrs=4
+else
+	empHrs=0
 fi
+	salary=$(( $empHrs*$EMP_RATE_PER_HOURS ))
 
 # Employee wage using case Statement
-
 random=$(( RANDOM%2 ))
 employeeType=$(( RANDOM%2 ))
 case "$random" in
 # case 0 is employee present else absent
-0)
-	 
-	if [ $employeeType -eq $employee_Part_Time_Hours ]
-	then
-		echo "Employee is present and this is  Full time worker . "
-		echo "Employee  daily wage is  : $(( $emp_Rate_per_Hours*$employee_Full_Time_Hours )) "
-	else
-		echo "Employee is present and this is part time worker . "
-		echo "Employee  daily wage is : $(( $emp_Rate_per_Hours*$employee_Part_Time_Hours )) " 
-	fi
-   ;;
-1)
-	echo "Employee is absent :"
-   ;;
+	0) 
+		if [ $employeeType -eq $EMP_PART_TIME_HOURS ]
+		then
+			echo "Employee is present and this is  Full time worker . "
+			echo "Employee  daily wage is  : $(( $EMP_RATE_PER_HOURS*$EMP_FULL_TIME_HOURS )) "
+		else
+			echo "Employee is present and this is part time worker . "
+			echo "Employee  daily wage is : $(( $EMP_RATE_PER_HOURS*$EMP_PART_TIME_HOURS )) " 
+		fi
+   	;;
+	1)
+		echo "Employee is absent :"
+   	;;
 esac
 
 
 # Calculating Wage for a month
 
-for (( day=1; day<=$num_Working_Days; day++ ))
+for (( day=1; day<=$NUM_WORKING_DAYS; day++ ))
 do
-   random=$(( RANDOM%3 ))
+	random=$(( RANDOM%3 ))
    case "$random" in
-      1)
+   	1)
 	      empHrs=8 
 			;;
       2)
@@ -69,10 +76,10 @@ do
 	      empHrs=0 
 	      ;;
    esac               
-    salary=$(($empHrs * $emp_Rate_per_Hours))
+   salary=$(($empHrs * $EMP_RATE_PER_HOURS))
    totalSalary=$(($totalSalary+$salary))
- echo "normal salary  : $salary"
 done
+
 
 # Wages Working Hours and days
 
@@ -84,87 +91,86 @@ do
 
 # Random function generate 100 then run if block And calculate oprations
 case "$hoursDays" in 
-0)
-   employeeType=$(( RANDOM%2 ))
-	echo "Employee worked 100 hour"
-	if [ $employeeType -eq 0 ]
-	then
-	   echo "Employee is full time worker ."
-		echo "Employee wages is $(( $mx_Hrs_In_month*$emp_Rate_per_Hours )) "
-		loop=0
-	else
-		echo "Employee is part  time worker ."
-      echo "Employee wages is $(( $mx_Hrs_In_month*$emp_Rate_per_Hours )) "
-		loop=0
-	fi
-	;;
+	0)
+   	employeeType=$(( RANDOM%2 ))
+		echo "Employee worked 100 hour"
+		if [ $employeeType -eq $IS_FULL_TIME ]
+		then
+	   	echo "Employee is full time worker ."
+			echo "Employee wages is $(( $MX_HRS_IN_MONTH*$EMP_RATE_PER_HOURS )) "
+			loop=0
+		else
+			echo "Employee is part  time worker ."
+      	echo "Employee wages is $(( $MX_HRS_IN_MONTH*$EMP_RATE_PER_HOURS )) "
+			loop=0
+		fi
+		;;
 
 # Random function generate 100 then run if block And calculate oprations 
-1)
-   employeeType=$(( RANDOM%2 ))
-   echo "Employee worked 20 days"
-   if [ $employeeType -eq 0 ]
-   then
-      echo "Employee is full time worker ."
-      echo "Employee wages is $(( $(( $num_Working_Days*$employee_Full_Time_Hours )) * $emp_Rate_per_Hours )) "
-	   loop=0
-   else
-      echo "Employee is part  time worker ."
-      echo "Employee wages is $(( $(( $num_Working_Days*$employee_Part_Time_Hours )) * $emp_Rate_per_Hours ))"
-	   loop=0
-   fi
-	;;
+	1)
+   	employeeType=$(( RANDOM%2 ))
+   	echo "Employee worked 20 days"
+   	if [ $employeeType -eq $IS_FULL_TIME ]
+   	then
+      	echo "Employee is full time worker ."
+      	echo "Employee wages is $(( $(( $NUM_WORKING_DAYS*$EMP_FULL_TIME_HOURS )) * $EMP_RATE_PER_HOURS )) "
+	   	loop=0
+   	else
+      	echo "Employee is part  time worker ."
+      	echo "Employee wages is $(( $(( $NUM_WORKING_DAYS*$EMP_PART_TIME_HOURS )) * $EMP_RATE_PER_HOURS ))"
+	   	loop=0
+   	fi
+		;;
 esac
 
 done
+
 
 # Work hours using Function 
 
 function getworkHourse(){
 
-	if [ $1 -eq 0 ]
+	if [ $1 -eq 2 ]
 	then
-		echo "$employee_Part_Time_Hours"
+		echo "$employee_hours"
 
-	elif [ $1 -eq 1 ]
+	elif [ $1 -eq $IS_FULL_TIME ]
 	then
-		echo "$employee_Full_Time_Hours"
+		echo "$EMP_FULL_TIME_HOURS"
 
-	elif [ $1 -eq 2 ]
-   then
-		echo "employee_hours"
+	elif [ $1 -eq $IS_PART_TIME ]
+    then
+		echo "$EMP_PART_TIME_HOURS"
 	fi
 }
   
 random=$(( RANDOM%3 ))
 employeeType="$( getworkHourse $random )"
-echo "$employeeType"
 
 
 # find working days
 function getworkHourse(){
 
-	if [ $1 -eq 0 ]
+	if [ $1 -eq 2 ]
 	then
-		echo "$employee_Part_Time_Hours"
-
-	elif [ $1 -eq 1 ]
-	then
-		echo "$employee_Full_Time_Hours"
-
-	elif [ $1 -eq 2 ]
-   then
 		echo "$employee_hours"
+
+	elif [ $1 -eq $IS_FULL_TIME ]
+	then
+		echo "$EMP_FULL_TIME_HOURS"
+
+	elif [ $1 -eq $IS_PART_TIME ]
+    then
+		echo "$EMP_PART_TIME_HOURS"
 	fi
 }
-
 # find wage 
 function dailyWage(){
 
-	wage=$(( $1*$emp_Rate_per_Hours ))
+	wage=$(( $1*$EMP_RATE_PER_HOURS ))
 	echo "$wage"
 }
-while [[ $totalWorkHours -lt $mx_Hrs_In_month && $totalWorkingDays -lt $num_Working_Days ]]
+while [[ $totalWorkHours -lt $MX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
 	((totalWorkingDays++))
 	hour="$( getworkHourse $(( RANDOM%3 )) )"
@@ -174,16 +180,18 @@ done
 
 	totalSalary="$( dailyWage $totalWorkHours )"
 	echo "Daily Wage " ${empDailyWage[@]}
+
+
 # find working days
 function getworkHourse(){
 
-   if [ $1 -eq 0 ]
+   if [ $1 -eq $IS_PART_TIME ]
    then
-      echo "$employee_Part_Time_Hours"
+      echo "$EMP_PART_TIME_HOURS"
 
-   elif [ $1 -eq 1 ]
+   elif [ $1 -eq $IS_FULL_TIME ]
    then
-      echo "$employee_Full_Time_Hours"
+      echo "$EMP_FULL_TIME_HOURS"
 
    elif [ $1 -eq 2 ]
    then
@@ -193,11 +201,9 @@ function getworkHourse(){
 
 # find wage 
 function dailyWage(){
-
-   wage=$(( $1*$emp_Rate_per_Hours ))
-   echo "$wage"
+   wage=$(( $1*$EMP_RATE_PER_HOURS ))
 }
-while [[ $totalWorkHours -lt $mx_Hrs_In_month && $totalWorkingDays -lt $num_Working_Days ]]
+while [[ $totalWorkHours -lt $MX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
    ((totalWorkingDays++))
    hour="$( getworkHourse $(( RANDOM%3 )) )"
@@ -208,3 +214,4 @@ done
    totalSalary="$( dailyWage $totalWorkHours )"
    echo "Daily Wage " ${empDailyWage[@]}
 	echo "All keys " ${!empDailyWage[@]}
+
